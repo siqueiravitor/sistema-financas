fbc162b36cbc89ea09df623b54b4c5789a846d27
siqueiravitor <siqueiravitor@outlook.com.br>
2023-08-19 22:11:36 -0300

    feat: Breadcrumb link

--- app/include/breadcrumb.php
+++ app/include/breadcrumb.php
@@ -10,10 +10,14 @@
                         $removeGet = explode('?', $_SERVER['REQUEST_URI'])[0];
                         $breadcrumbs = explode('/', $removeGet);
                     }
-
+                    $lastIdx= array_key_last($breadcrumbs)-1;
                     foreach ($breadcrumbs as $idx => $breadcrumb) {
                         if ($idx > 2 && $breadcrumb) {
-                            echo "<li class='breadcrumb-item'>$breadcrumb</li> ";
+                            if($lastIdx != $idx){
+                                echo "<li class='breadcrumb-item'><a href='".BASED."/$breadcrumb'>$breadcrumb</a></li>";
+                            } else {
+                                echo "<li class='breadcrumb-item active'>$breadcrumb</li>";
+                            }
                         }
                     }
                     ?>
