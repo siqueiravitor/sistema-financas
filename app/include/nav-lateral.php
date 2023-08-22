0803c3a11640d91eec4d82df1b2f628af2d33e0a
Vitor Siqueira <RiotMagnus@outlook.com>
2023-08-22 13:03:06 -0300

    fix: Sidebar responsiveness

--- app/include/nav-lateral.php
+++ app/include/nav-lateral.php
@@ -1,5 +1,5 @@
 <div id="leftsidebar" class="sidebar bg-gradient">
-    <div class="sidebar-scroll">
+    <div id='sidebar-scroll' class="sidebar-scroll">
         <nav id="leftsidebar-nav" class="sidebar-nav mt-3">
             <ul id="main-menu" class="metismenu">
                 <?php
@@ -13,7 +13,7 @@
 
                 $active = empty(explode('/', $_SERVER['REQUEST_URI'])[3]) ? 'active' : '';
                 ?>
-                <li class="<?= $active ?>">
+                <li class="middle-sm-hide <?= $active ?>">
                     <a href="<?= BASED; ?>">
                         <i class='iconColor navIcon' data-feather="bar-chart-2"></i>
                         <span class='ml-2'>Dashboard</span>
@@ -34,7 +34,7 @@
                 ];
                 ?>
                 <li class="heading">Menu</li>
-                <li class="middle" id="navMenuUsuario">
+                <li class="middle submenu-sm-hide" id="navMenuUsuario">
                     <a class="has-arrow">
                         <i class='iconColor navIcon' data-feather="user"></i>
                         <span class='ml-2'><?= $nome ?></span>
@@ -55,49 +55,49 @@
                     </ul>
                 </li>
                 <li class="divider"></li>
-                <li class="middle <?= 'sistema' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'sistema' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/sistema" ?>">
                         <i class='iconColor navIcon' data-feather="cpu"></i>
                         <span class='ml-2'>Sistema</span>
                     </a>
                 </li>
-                <li class="middle <?= 'lancamentos' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'lancamentos' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/lancamentos" ?>">
                         <i class='iconColor navIcon' data-feather="edit"></i>
                         <span class='ml-2'>Lançamentos</span>
                     </a>
                 </li>
-                <li class="middle <?= 'poupanca' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'poupanca' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/poupanca" ?>">
                         <i class='iconColor navIcon' data-feather="pocket"></i>
                         <span class='ml-2'>Poupança</span>
                     </a>
                 </li>
-                <li class="middle <?= 'grupos' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'grupos' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/grupos" ?>">
                         <i class='iconColor navIcon' data-feather="grid"></i>
                         <span class='ml-2'>Grupos</span>
                     </a>
                 </li>
-                <li class="middle <?= 'listas' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'listas' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/listas" ?>">
                         <i class='iconColor navIcon' data-feather="list"></i>
                         <span class='ml-2'>Listas</span>
                     </a>
                 </li>
-                <li class="middle <?= 'categorias' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'categorias' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/categorias" ?>">
                         <i class='iconColor navIcon' data-feather="command"></i>
                         <span class='ml-2'>Categorias</span>
                     </a>
                 </li>
-                <li class="middle <?= 'tipopagamento' == $page ? 'active' : '' ?>">
+                <li class="middle middle-sm-hide <?= 'tipopagamento' == $page ? 'active' : '' ?>">
                     <a href="<?= BASED . "/tipopagamento" ?>">
                         <i class='iconColor navIcon' data-feather="dollar-sign"></i>
                         <span class='ml-2'>Tipo Pagamento</span>
                     </a>
                 </li>
-                <li class="middle" id="navDocumentacao">
+                <li class="middle middle-sm-hide" id="navDocumentacao">
                     <a href="<?= BASED ?>/sobre">
                         <i class="icon-info"></i>
                         <span>Sobre</span>
@@ -105,7 +105,7 @@
                 </li>
             </ul>
         </nav>
-        <div class="fixed-bottom">
+        <div class="side-bottom">
             <?= VERSION ?>
         </div>
     </div>
